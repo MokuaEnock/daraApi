289aca97a1bcd2f00d3f93b0196ab84fe8acc9de
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 23:53:11 +0300

    create auth header method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
   def encode_token(payload)
+    JWT.encode(payload, "my_s3cr3t")
+  end
+
+  def auth_header
+
   end
 
   def decode_token(token)
+    JWT.decode(token, "my_s3cr3t"[0])
   end
 end
