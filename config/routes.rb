af4912f159f50b3646fbe48ca0b543ec1ad85cd4
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 22:39:29 +0300

    create route for user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: %i[show create]
+  post "/login", to: "sessions#create"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
