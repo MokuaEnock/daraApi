d74a057024a6db44d79fb0efeac36cdf87fb4234
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 23:35:40 +0300

    create users profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: %i[show create]
   post "/login", to: "sessions#create"
-  
+  get "/profile", to: "users#profile"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
