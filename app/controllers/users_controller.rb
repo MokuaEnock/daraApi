3ed5d0fff11072409bb1ee85f3b4fad5bf970146
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 23:34:20 +0300

    update code for user create action to accomodate JWts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def create
-    user = User.create(user_params)
-    render json: user
+    @user = User.create(user_params)
+    if @user.valid?
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: {
+               error: "Failed to create user"
+             },
+             status: :unprocessable_entity
+    end
   end
 
   private
