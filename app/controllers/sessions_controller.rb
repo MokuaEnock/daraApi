a5af8e884fd30b3b6e31cab8b7467819a2e08b9c
Enock <enock.mokua@student.moringaschool.com>
2022-12-29 22:45:13 +0300

    add create action for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def create
-    
+    user = user.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user, status: :created
+    else
+      render json: {
+               error: {
+                 login: "Invalid email or password"
+               }
+             },
+             status: :unauthorized
+    end
   end
 end
