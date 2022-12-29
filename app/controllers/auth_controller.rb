4d218d23f756087540e4b96d88270f77711bb883
Enock <enock.mokua@student.moringaschool.com>
2022-12-30 00:38:33 +0300

    create login method

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,6 +2,21 @@ class AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
+    @user = User.find_by(email: login_params[:email])
+
+    if @user && @user.authenticate(login_params[:password])
+      token = encode_token ({ user_id: @user.id })
+      render json: {
+               user: UserSerializer.new(@user),
+               jwt: token
+             },
+             status: :accepted
+    else
+      render json: {
+               message: "Invalid Email or password"
+             },
+             status: :unauthorized
+    end
   end
 
   private
