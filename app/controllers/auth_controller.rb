369bb0453bc6ca2952888332d801beb8ed32f1b6
Enock <enock.mokua@student.moringaschool.com>
2022-12-30 00:31:52 +0300

    add create and login_params methods

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,12 @@
 class AuthController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def create
+  end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
+  end
 end
