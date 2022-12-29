fe473c9ce912aec27bc6eee151aaa9133bd665e3
Enock <enock.mokua@student.moringaschool.com>
2022-12-30 00:01:57 +0300

    add current user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,26 @@ class ApplicationController < ActionController::API
   end
 
   def auth_header
-
+    request.headers["Authorization"]
   end
 
-  def decode_token(token)
-    JWT.decode(token, "my_s3cr3t"[0])
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' '[1])
+      begin
+        JWT.decode(token, "my_s3cr3t", true, algorithm: "HS256")
+      rescue JWT::DecodeError
+        nil
+      end
+    end
   end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]["user_id"]
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  
 end
