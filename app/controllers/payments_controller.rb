18ea08491871692cd330acd2f6ee923dba9bb10c
Enock <enock.mokua@student.moringaschool.com>
2023-01-03 02:06:20 +0300

    add mpesa methods

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -10,6 +10,44 @@ class PaymentsController < ApplicationController
     render json: pay
   end
 
+  def mpesa_index
+    token = get_access_token
+
+    url = URI("https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest")
+
+    https = Net::HTTP.new(url.host, url.port)
+    https.use_ssl = true
+
+    request = Net::HTTP::Post.new(url)
+    request["Authorization"] = "Bearer #{token}"
+    request["Content-Type"] = "application/json"
+    password =
+      Base64.strict_encode64(
+        "174379bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919#{Time.now.strftime "%Y%m%d%H%M%S"}"
+      )
+
+    request.body = {
+      BusinessShortCode: 174_379,
+      Password: password,
+      Timestamp: "#{Time.now.strftime "%Y%m%d%H%M%S"}",
+      TransactionType: "CustomerPayBillOnline",
+      Amount: 100,
+      PartyA: params[:phone_number],
+      PartyB: 174_379,
+      PhoneNumber: params[:phone_number],
+      CallBackURL: "https://hired-app-api.herokuapp.com/mpesa_callback",
+      AccountReference: "Hired LTD",
+      TransactionDesc: "Payment of subscription"
+    }.to_json
+
+    response = https.request(request)
+    render json: response.body
+  end
+
+  def mpesa_callback
+    render json: params[:Body].to_json
+  end
+
   private
 
   def pay_params
