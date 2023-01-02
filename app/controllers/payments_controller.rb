8a2d564460a3dd7186fcc1f9a2e8b3efc5a7da08
Enock <enock.mokua@student.moringaschool.com>
2023-01-03 00:30:14 +0300

    add index and create actions for payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,10 @@
 class PaymentsController < ApplicationController
+  def index
+    render json: Payment.all
+  end
+
+  def create
+    pay = Payment.create(pay_params)
+    render json: pay
+  end
 end
