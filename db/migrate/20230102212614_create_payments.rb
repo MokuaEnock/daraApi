f0aa182e5bfa64db806fd8454fa9c209b1ce37fd
Enock <enock.mokua@student.moringaschool.com>
2023-01-03 00:28:22 +0300

    add payments attributes and update schema

--- db/migrate/20230102212614_create_payments.rb
+++ db/migrate/20230102212614_create_payments.rb
@@ -1,6 +1,8 @@
 class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
+      t.integer :phone
+      t.integer :total
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_175716) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_212614) do
+  create_table "payments", force: :cascade do |t|
+    t.integer "phone"
+    t.integer "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
