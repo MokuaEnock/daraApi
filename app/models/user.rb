5644f4a793df9910a76db501934396bc93018f99
Enock <enock.mokua@student.moringaschool.com>
2023-01-04 02:33:24 +0300

    add devise configuration in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  devise :database_authenticatable,
+         :timeoutable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :omniauthable,
+         omniauth_providers: [:google_oauth2]
 end
