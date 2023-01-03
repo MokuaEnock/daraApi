53f1db18c397142e77080acfd7a7ad9318dbb8cc
Enock <enock.mokua@student.moringaschool.com>
2023-01-04 02:50:07 +0300

    add attributes to users

--- db/migrate/20230103234311_create_users.rb
+++ db/migrate/20230103234311_create_users.rb
@@ -1,8 +1,20 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :email, null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+      t.string :full_name
+      t.string :uid
+      t.string :avatar_url
+      t.string :provider
 
-      t.timestamps
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.timestamps null: false
+
+      add_index :users, :email, unique: true
+      add_index :users, :reset_password_token, unique: true
     end
   end
 end
